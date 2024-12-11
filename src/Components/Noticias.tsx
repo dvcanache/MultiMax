adbd72ca1ee24e0e93a3d5258bab716d540a3edf
Dave <davehcanache@gmail.com>
2024-12-11 00:10:02 -0400

    Componente Noticias.tsx corregido

--- src/Components/Noticias.tsx
+++ src/Components/Noticias.tsx
@@ -1,4 +1,4 @@
-const Recomendados = () => {
+const Noticias = () => {
   return (
     <div>
 
@@ -6,4 +6,4 @@ const Recomendados = () => {
   )
 };
 
-export default Recomendados;
+export default Noticias;
