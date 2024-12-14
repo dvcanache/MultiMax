c444ead5712f398b6e5192bb2973d90a3722ca6d
Dave Canache <106845716+dvcanache@users.noreply.github.com>
2024-12-14 14:00:00 -0400

    Panel Remodelado, De Boton Animacion Corregida, Filtros agregados a la barra lateral

--- src/Components/HeroSection/BarraLateral.tsx
+++ src/Components/HeroSection/BarraLateral.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 import { Bars3Icon } from "@heroicons/react/24/outline";
+import { FilterProducts, NavBarMenu } from "../../mockData/data";
+import { FaFilter } from "react-icons/fa";
+
+const LogoUrl = `https://multimax.com.ve/wp-content/uploads/2024/11/Logo-Multimax-web.png`;
 
 interface BarraLateralProps {
   isOpen: boolean;
@@ -21,9 +25,9 @@ const BarraLateral: React.FC<BarraLateralProps> = ({
       {/* Botón para abrir/cerrar barra lateral */}
       <button
         className={`fixed ${
-          estaScroll ? "top-20 left-0 rounded-r-full" : "top-20 left-8 rounded"
-        } bg-blue-500 text-white px-1 py-2 shadow-md hover:bg-blue-700 focus:outline-none z-20 flex items-center justify-center transition-all duration-500 
-        ${estaScroll ? "md:left-0" : "md:left-8"} hidden md:flex`}
+          estaScroll ? "top-20 left-0 rounded-r-full" : "top-20 left-8 rounded-r-full"
+        } bg-blue-500 text-white px-1 py-2 shadow-md hover:bg-blue-700 focus:outline-none z-20 flex items-center justify-center transition-all duration-300 ease-in
+        ${estaScroll ? "md:left-0 w-10" : "md:left-8 w-20"} hidden md:flex`}
         style={{
           transitionProperty: "all, border-radius",
         }}
@@ -31,7 +35,7 @@ const BarraLateral: React.FC<BarraLateralProps> = ({
       >
         <Bars3Icon className="h-6 w-6 mr-2" />
         <span className={`hidden md:block ${estaScroll ? "md:hidden" : ""}`}>
-          {!estaScroll && "Todo"}
+          Todo
         </span>
       </button>
 
@@ -51,13 +55,21 @@ const BarraLateral: React.FC<BarraLateralProps> = ({
 
       {/* Barra lateral */}
       <div
-        className={`fixed top-0 left-0 h-full w-64 bg-blue-500 text-white transform ${
+        className={`fixed top-0 left-0 h-full w-64 bg-white text-white transform ${
           isOpen ? "translate-x-0" : "-translate-x-full"
         } transition-transform duration-300 ease-in-out z-50`}
         onClick={(e) => e.stopPropagation()}
       >
-        <div className="p-4 flex justify-between items-center">
-          <h2 className="text-lg font-bold">Barra Lateral</h2>
+        <div className="p-4 flex justify-between items-center bg-blue-500">
+          <div className="text-2xl flex items-center">
+            <a href={NavBarMenu[0].link} className="min-w-[40px]">
+              <img
+                src={LogoUrl}
+                alt="Multimax Logo"
+                className="object-cover h-10"
+              />
+            </a>
+          </div>
           <button
             className="text-white hover:text-gray-400 focus:outline-none"
             onClick={onClose}
@@ -65,13 +77,22 @@ const BarraLateral: React.FC<BarraLateralProps> = ({
             ✕
           </button>
         </div>
-        <div className="p-4">
-          <ul className="space-y-2">
-            <li className="hover:bg-gray-700 p-2 rounded">Opción 1</li>
-            <li className="hover:bg-gray-700 p-2 rounded">Opción 2</li>
-            <li className="hover:bg-gray-700 p-2 rounded">Opción 3</li>
-          </ul>
-        </div>
+          <div className="w-full bg-blue-400 text-white p-3.5 font-semibold text-lg inline-flex justify-between">
+            Filtros
+            <FaFilter />
+          </div>
+        <ul className="">
+          <div className="overflow-y-auto h-[calc(100vh-130px)]">
+          {FilterProducts.map((item) => (
+            <li
+              key={item.id}
+              className="bg-white text-gray-800 hover:bg-gray-200 py-3.5 px-4 cursor-pointer"
+            >
+              <a className=" font-semibold text-md text-nowrap">{item.type}</a>
+            </li>
+          ))}
+          </div>
+        </ul>
       </div>
 
       {/* Contenido principal */}
